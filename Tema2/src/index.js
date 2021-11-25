3d7585bcd7b91c448b84026c250ce359a0ff640e
Oliver Aparicio Martinez <olivermtz35@gmail.com>
2021-11-25 16:34:08 -0600

    Subida de ficheros

---
+++ Tema2/src/index.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+function componente(){
+    const elemento = document.createElement('div');
+    elemento.innerHTML = _.join(['Hola', 'Webpack'], ' ');
+    return elemento;
+}
+document.body.appendChild(componente());
